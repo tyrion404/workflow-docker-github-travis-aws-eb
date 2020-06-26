113d0a5ff828fb8ced88ff2f739dde1f74f0e70c
tyrion404 <chintan.170410116089@gmail.com>
2020-06-26 09:38:26 +0530

    CTD:Ia0 initial

---
+++ src/component/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      This project is created to understand the workflow of
+      Docker-GitHub-Travis-AWS-elastic-beanstalk!
+    </div>
+  );
+}
+
+export default App;
