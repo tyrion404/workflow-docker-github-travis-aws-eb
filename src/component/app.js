cb1db695cdd7eaedb83723798c3e982dedf24c30
tyrion404 <chintan.170410116089@gmail.com>
2020-06-27 20:00:12 +0530

    CTD:Ua8 Adding Style to the App

--- src/component/app.js
+++ src/component/app.js
@@ -3,8 +3,14 @@ import React from 'react';
 function App() {
   return (
     <div>
-      This project is created to understand the workflow of
-      Docker-GitHub-Travis-AWS-elastic-beanstalk!
+      <h3 style={{ textAlign: 'center' }}>
+        This project is created to understand the workflow of
+        Docker-GitHub-Travis-AWS-elastic-beanstalk!
+      </h3>
+      <div>
+        <img alt='workflow' src='workflow.png' width='100%' />
+        Flow of deploying this application
+      </div>
     </div>
   );
 }
